75adf386ce2e353f17b1ee82c6c6ce7b16762020
wilson-castro <wilsoncastro.pv@gmail.com>
2021-08-25 08:39:50 -0300

    Aplicação TodoApp sem o Redux para o tratamento de estado

---
+++ todo-app/frontend/src/about/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PageHeader from "../template/PageHeader";
+
+export default props => (
+  <div>
+    <PageHeader name="Sobre" small="Nós" />
+
+    <h2>Nossa História</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur...</p>
+    <h2>Missão e visão</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur...</p>
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur...</p>
+  </div>
+)
---
+++ todo-app/frontend/src/todo/TodoForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Grid from '../template/Grid'
+import IconButton from '../template/IconButton'
+export default props => {
+  const keyHandler = (event) => {
+    if (event.key === 'Enter') {
+      event.shiftKey ? props.handleSearch() : props.handleAdd()
+    } else if (event.key === 'Escape') {
+      props.handleClear()
+    }
+  }
+
+  return (
+
+    <div role="form" className="todoForm">
+      <Grid cols="12 9 10">
+        <input id="description"
+          onChange={props.handleChange}
+          onKeyUp={keyHandler}
+          value={props.description} className="form-control" placeholder="Adicione uma tarefa" />
+      </Grid>
+      <Grid cols="12 3 2">
+        <IconButton style='primary' icon='plus'
+          onClick={props.handleAdd}></IconButton>
+        <IconButton style='info' icon='search'
+          onClick={props.handleSearch}></IconButton>
+        <IconButton style='default' icon='close'
+          onClick={props.handleClear}></IconButton>
+      </Grid>
+    </div>
+  )
+}
