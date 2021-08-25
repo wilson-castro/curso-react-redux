b8f66fd82ed13a160223d45de9c8ea61fc9f73f2
wilson-castro <wilsoncastro.pv@gmail.com>
2021-08-25 17:10:33 -0300

    Conclusão do frontend e do exemplo TodoApp

--- todo-app/frontend/src/todo/TodoList.jsx
+++ todo-app/frontend/src/todo/TodoList.jsx
@@ -1,33 +1,35 @@
 import React from 'react'
-import IconButton from '../template/IconButton'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
-export default props => {
+import IconButton from '../template/IconButton'
+import { markAsDone, markAsPending, remove } from './todoActions'
+
+const TodoList = props => {
 
   const renderRows = () => {
     const list = props.list || []
-
-    return list.map((todo) => (
+    return list.map(todo => (
       <tr key={todo._id}>
         <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
         <td>
-          <IconButton style="success" icon="check" hide={todo.done}
-            onClick={() => props.handleMarkAsDone(todo)}></IconButton>
-          <IconButton style="warning" icon="undo" hide={!todo.done}
-            onClick={() => props.handleMarkAsPending(todo)}></IconButton>
-          <IconButton style="danger" icon="trash-o" hide={!todo.done}
-            onClick={() => props.handleRemove(todo)}></IconButton>
-
+          <IconButton style='success' icon='check' hide={todo.done}
+            onClick={() => props.markAsDone(todo)}></IconButton>
+          <IconButton style='warning' icon='undo' hide={!todo.done}
+            onClick={() => props.markAsPending(todo)}></IconButton>
+          <IconButton style='danger' icon='trash-o' hide={!todo.done}
+            onClick={() => props.remove(todo)}></IconButton>
         </td>
       </tr>
     ))
   }
 
   return (
-    <table className="table">
+    <table className='table'>
       <thead>
         <tr>
           <th>Descrição</th>
-          <th className="tableActions">Ações</th>
+          <th className='tableActions'>Ações</th>
         </tr>
       </thead>
       <tbody>
@@ -36,3 +38,8 @@ export default props => {
     </table>
   )
 }
+
+const mapStateToProps = state => ({ list: state.todo.list })
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ markAsDone, markAsPending, remove }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
