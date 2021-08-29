353e03c2a45dc1f07dc119bdd5180f978bb77e49
wilson-castro <wilsoncastro.pv@gmail.com>
2021-08-29 19:17:38 -0300

    Conclusão da primeira parte do frontend e implementação do redux

---
+++ my-money-app/frontend/src/main/app.jsx
@@ -0,0 +1,18 @@
+import '../common/template/dependencies.js'
+import React from 'react'
+
+import Header from '../common/template/header.jsx'
+import SideBar from '../common/template/sideBar.jsx'
+import Footer from '../common/template/footer.jsx'
+import Routes from './routes.jsx'
+
+export default props => (
+  <div className="wrapper">
+    <Header />
+    <SideBar />
+    <div className="content-wrapper">
+      <Routes />
+    </div>
+    <Footer />
+  </div>
+)
---
+++ my-money-app/frontend/src/main/routes.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { Router, Route, Redirect, hashHistory } from 'react-router'
+
+// import Dashboard from '../dashboard/dashboard'
+import Dashboard from '../dashboard2/dashboard2'
+import BillingCycle from '../billingCycle/billingCycle'
+
+export default props => (
+  <Router history={hashHistory}>
+    <Route path="/" component={Dashboard} />
+    <Route path='/billingCycles' component={BillingCycle} />
+    <Redirect from='*' to='/' />
+  </Router>
+)
