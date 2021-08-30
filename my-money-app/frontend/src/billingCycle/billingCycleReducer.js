aca2cf269c38392a21676cf93067a6f784a96f29
wilson-castro <wilsoncastro.pv@gmail.com>
2021-08-30 16:58:01 -0300

    Atualização da interface continuidade com o uso do Redux

---
+++ my-money-app/frontend/src/billingCycle/billingCycleReducer.js
@@ -0,0 +1,15 @@
+import { actions } from "react-redux-toastr";
+
+const INITIAL_STATE = { list: [] }
+
+export default (state = INITIAL_STATE, action) => {
+
+  switch (action.type) {
+    case 'BILLING_CYCLES_FETCHED':
+
+      return { ...state, list: action.payload.data }
+
+    default:
+      return state
+  }
+}
