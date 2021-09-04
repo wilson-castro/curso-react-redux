583a73abc632aa36965bd60189c396f5b532b77a
wilson-castro <wilsoncastro.pv@gmail.com>
2021-09-04 11:26:53 -0300

    Construção do crud do usuário, alteração nas rotas, criação da tabela e implementação da responsividade

--- crud/frontend/src/components/template/Logo.jsx
+++ crud/frontend/src/components/template/Logo.jsx
@@ -3,11 +3,12 @@ import './Logo.css'
 import logo from '../../asseets/imgs/logo.png'
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props => (
   <aside className="logo">
-    <a href="/" className="logo">
+    <Link to="/" className="logo">
       <img src={logo} alt="logo" />
-    </a>
+    </Link>
   </aside>
 )
--- crud/frontend/src/components/template/Nav.jsx
+++ crud/frontend/src/components/template/Nav.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable import/no-anonymous-default-export */
 import './Nav.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props =>
   <aside className="menu-area">
     <nav className="menu">
-      <a href="#/">
+      <Link to="/">
         <i className="fa fa-home"></i> Início
-      </a>
-      <a href="#/users">
-        <i className="fa fa-users"></i> Usuários
-      </a>
+      </Link>
+      <Link to="/users">
+        <i clLinkssName="fa fa-users"></i> Usuários
+      </Link>
     </nav>
   </aside>
